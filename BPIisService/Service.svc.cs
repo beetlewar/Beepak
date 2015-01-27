2ca0bdc7a1febfa1214433dd036578374750dfdd
beetlewar <beetlewar@mail.ru>
2015-01-27 19:14:47 +0300

    2

--- BPIisService/Service.svc.cs
+++ BPIisService/Service.svc.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 
-namespace BPService
+namespace BPIisService
 {
     public class Service : IService
     {
