2fa945b9a6c237b129e80a9091dbafc395dc8c43
beetlewar <beetlewar@mail.ru>
2015-01-27 18:24:58 +0300

    Rename

--- BPService/Service.cs
+++ BPService/Service.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace BPService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class Service1 : IService1
+    public class Service : IService
     {
         public string GetData(int value)
         {
