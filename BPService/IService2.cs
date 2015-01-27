cf9af6e6401f2ffcd3d70f67e4ebc68d91a86f7a
beetlewar <beetlewar@mail.ru>
2015-01-27 18:28:54 +0300

    1

--- BPService/IService2.cs
+++ BPService/IService2.cs
@@ -9,7 +9,7 @@ namespace BPService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService
+    public interface IService2
     {
         [OperationContract]
         string GetData(int value);
