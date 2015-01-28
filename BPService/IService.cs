08617f9321995b4d7e5f48e555d1d4bbff5b83f7
beetlewar <beetlewar@mail.ru>
2015-01-28 11:07:16 +0300

    1

--- BPService/IService.cs
+++ BPService/IService.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
-namespace BPService
+namespace Beepak
 {
     [ServiceContract]
     public interface IService
---
+++ BPServiceHost/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beepak
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //var baseAddress = new Uri("http://localhost:8080/hello");
+            //using (var host = new ServiceHost(typeof(Service), baseAddress))
+            //{
+            //    host.Open();
+
+            //    Console.WriteLine("The service is ready at {0}", baseAddress);
+            //    Console.WriteLine("Press <Enter> to stop the service.");
+            //    Console.ReadLine();
+
+            //    host.Close();
+            //}
+        }
+    }
+}
