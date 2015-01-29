9e06da263aaeea0ae69bb8f9f0d0e16841f8665b
beetlewar <beetlewar@mail.ru>
2015-01-29 15:52:00 +0300

    Реализована регистрация пользователя

--- Beepak.Service/IService.cs
+++ Beepak.Service/IService.cs
@@ -5,14 +5,20 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using Beepak.Data;
 
-namespace Beepak
+namespace Beepak.Service
 {
     [ServiceContract]
     public interface IService
     {
         [OperationContract]
         [WebGet]
-        string GetData(int value);
+        User Register(
+            string login, 
+            string password, 
+            string passwordRpt,
+            string mail,
+            string city);
     }
 }
--- Beepak.ServiceHost/Program.cs
+++ Beepak.ServiceHost/Program.cs
@@ -6,15 +6,16 @@ using System.ServiceModel.Description;
 using System.ServiceModel.Web;
 using System.Text;
 using System.Threading.Tasks;
+using Beepak.Service;
 
-namespace Beepak
+namespace Beepak.ServiceHost
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string address = "http://localhost:8000";
-            using (var host = new WebServiceHost(typeof(Service), new Uri(address)))
+            using (var host = new WebServiceHost(typeof(ServiceImpl), new Uri(address)))
             {
                 try
                 {
@@ -27,9 +28,9 @@ namespace Beepak
 
                     host.Close();
                 }
-                catch (CommunicationException cex)
+                catch (CommunicationException ex)
                 {
-                    Console.WriteLine("An exception occurred: {0}", cex.Message);
+                    Console.WriteLine("An exception occurred: {0}", ex.Message);
                     Console.WriteLine("Press <Enter> to exit");
                     host.Abort();
                     Console.ReadLine();
