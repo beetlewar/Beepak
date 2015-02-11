a798571269a2c5ccf743895a2ad8c73f4c32fc93
beetlewar <beetlewar@mail.ru>
2015-02-11 16:29:58 +0300

    Реализовал подключение к БД по строке соединения из файла

--- src/Beepak.Data/ContextRepository/ContextRepositoryFactory.cs
+++ src/Beepak.Data/ContextRepository/ContextRepositoryFactory.cs
@@ -5,24 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using Beepak.Data.Context;
 using Beepak.Data.Decl;
+using System.Configuration;
 
 namespace Beepak.Data.ContextRepository
 {
     public class ContextRepositoryFactory :
         IRepositoryFactory
     {
-        public const string ConnectionString =
-            "data source=localhost\\MSSQLSERVER_12;" +
-            "initial catalog=Beepak;" +
-            "integrated security=True;" +
-            "multipleactiveresultsets=True;" +
-            "App=EntityFramework";
+        private static readonly string _connectionString;
+
+        static ContextRepositoryFactory()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
+        }
 
         public IRepository<T> CreateRepository<T>()
         {
             if(typeof(T) == typeof(User))
             {
-                return (IRepository<T>)new UsersRepository(ConnectionString);
+                return (IRepository<T>)new UsersRepository(_connectionString);
             }
 
             throw new NotImplementedException();
